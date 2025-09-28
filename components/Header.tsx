2d44af691bfbff033ee38104d7a150dbdaca7f4d
carlosvittor-ui <carlos.h.vittor@gmail.com>
2025-09-28 18:34:44 -0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -63,8 +63,8 @@ const Header: React.FC<HeaderProps> = ({ currentView, setCurrentView }) => {
     ];
 
     return (
-        <header className="fixed top-0 left-0 right-0 bg-white dark:bg-gray-800 shadow-md z-40 px-4 py-2">
-            <div className="max-w-7xl mx-auto flex justify-between items-center">
+        <header className="fixed top-0 left-0 right-0 bg-white dark:bg-gray-800 shadow-md z-40 px-4  h-16">
+            <div className="max-w-7xl mx-auto flex justify-between items-center h-full">
                 <div className="flex items-center space-x-2">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-indigo-600" viewBox="0 0 20 20" fill="currentColor">
                         <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
